db9ab4184a716d5f8a038c35d2410982b03a5a9b
Poojamoukhede <poojamoukhede27@gmail.com>
2023-09-16 17:40:00 +0530

    attandance with userRef

--- Model/Android/User.js
+++ Model/Android/User.js
@@ -1,7 +1,7 @@
 const mongooose = require("mongoose");
 
 const userSchema = new mongooose.Schema({
-  _id:{type:mongooose.Schema.Types.ObjectId}, // by this we archive useRef 
+  // _id:{type:mongooose.Schema.Types.ObjectId}, // by this we archive useRef 
   name: { type: String },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
