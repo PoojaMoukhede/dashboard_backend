2f74af4fdf3b1c47832b3e653771d0be1967b741
Poojamoukhede <poojamoukhede27@gmail.com>
2023-10-11 15:14:05 +0530

    trying to get images with extended size

--- index.js
+++ index.js
@@ -36,8 +36,12 @@ mongoose
   });
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false ,limit: '50mb'}));
 app.use(bodyParser.json());
+// app.use(bodyParser.json({limit: '50mb'}));
+// app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   next();
